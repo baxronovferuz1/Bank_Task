7532bc8c6f51ba313adb29a9ec1292eb77412b31
baxronovferuz1 <baxronovvferuz25@gmail.com>
2025-09-26 11:15:19 +0500

    write function account and transactions

--- utils/mockdata_db.py
+++ utils/mockdata_db.py
@@ -15,7 +15,7 @@ from datetime import datetime, timedelta
 fake = Faker()
 
 # Data basega  ulanish
-conn = sqlite3.connect('bank.db')
+conn = sqlite3.connect('task.db')
 cur = conn.cursor()
 
 # Tablitsalani yaratish qismi
@@ -43,10 +43,31 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Transactions (
     FOREIGN KEY(account_id) REFERENCES Accounts(id)
 )''')
 
-# Mock data: 100ming mijoz
+
 num_clients = 100000
 clients = []
 for i in range(1, num_clients + 1):
     clients.append((i, fake.name(), fake.date_of_birth(minimum_age=18, maximum_age=80), random.choice(['Toshkent', 'Jizzax', 'Samarqand', 'Buxoro', 'Xorazm', 'Andijon', 'Sirdaryo'])))
 
 pd.DataFrame(clients, columns=['id', 'name', 'birth_date', 'region']).to_sql('Clients', conn, if_exists='replace', index=False)
+
+
+num_accounts = 500000
+accounts = []
+for i in range(1, num_accounts + 1):
+    accounts.append((i, random.randint(1, num_clients), random.uniform(1000, 100000), fake.date_between(start_date='-5y', end_date='today')))
+
+pd.DataFrame(accounts, columns=['id', 'client_id', 'balance', 'open_date']).to_sql('Accounts', conn, if_exists='replace', index=False)
+
+
+num_transactions = 1000000
+transactions = []
+for i in range(1, num_transactions + 1):
+    date = fake.date_between(start_date='-1y', end_date='today')
+    transactions.append((i, random.randint(1, num_accounts), random.uniform(-5000, 5000), date, random.choice(['deposit', 'withdraw', 'transfer'])))
+
+pd.DataFrame(transactions, columns=['id', 'account_id', 'amount', 'date', 'type']).to_sql('Transactions', conn, if_exists='replace', index=False)
+
+conn.commit()
+conn.close()
+print("DB created! File: task.db")

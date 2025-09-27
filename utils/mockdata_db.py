74c56c7e4eaf863b6dc519ffe606af867b3217b0
baxronovferuz1 <baxronovvferuz25@gmail.com>
2025-09-27 14:20:19 +0500

    added 12 region,wrote sql_query function and export_to_excel function

--- utils/mockdata_db.py
+++ utils/mockdata_db.py
@@ -40,7 +40,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Transactions (
 num_clients = 1000
 clients = []
 for i in range(1, num_clients + 1):
-    clients.append((i, fake.name(), fake.date_of_birth(minimum_age=18, maximum_age=80), random.choice(['Toshkent', 'Jizzax', 'Samarqand', 'Buxoro', 'Xorazm', 'Andijon', 'Sirdaryo'])))
+    clients.append((i, fake.name(), fake.date_of_birth(minimum_age=18, maximum_age=80), random.choice(["Toshkent","Jizzax","Samarqand","Buxoro","Xorazm","Andijon","Sirdaryo","Navoiy","Surxandaryo","Qashqadaryo","Fargona","Namangan"])))
 
 pd.DataFrame(clients, columns=['id', 'name', 'birth_date', 'region']).to_sql('Clients', conn, if_exists='replace', index=False)
 
---
+++ utils/sql_query_and_excel_export.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import sqlite3
+import openpyxl
+from openpyxl.chart import BarChart, Reference
+
+
+
+def sql_query(sql):
+    conn = sqlite3.connect('task.db')
+    try:
+        df = pd.read_sql_query(sql, conn)
+    except Exception as e:
+        return pd.DataFrame({"error": [str(e)]})
+    conn.close()
+    return df
+
+def export_to_excel(df, filename='result.xlsx'):
+    df.to_excel(filename, index=False, sheet_name='Data')
+    
+    
+    wb = openpyxl.load_workbook(filename)
+    ws = wb['Data']
+    
+    if len(df) > 1 and len(df.columns) >= 2:  
+        chart = BarChart()
+        data = Reference(ws, min_col=2, min_row=1, max_col=2, max_row=len(df)+1)
+        cats = Reference(ws, min_col=1, min_row=2, max_row=len(df)+1)
+        chart.add_data(data, titles_from_data=True)
+        chart.set_categories(cats)
+        chart.title = "Natijalar Grafigi"
+        ws.add_chart(chart, "E5")
+    
+    wb.save(filename)
+    print(f"Excel saqlandi: {filename}")
+
+# Test
+if __name__ == "__main__":
+    sql = "SELECT region, SUM(balance) FROM Clients JOIN Accounts ON Clients.id = Accounts.client_id GROUP BY region"
+    df = sql_query(sql)
+    export_to_excel(df)
